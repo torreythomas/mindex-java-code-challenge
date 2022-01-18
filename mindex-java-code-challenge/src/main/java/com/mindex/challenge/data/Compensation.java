ad1b54a59ad53e2890ff08baa3544b73079fa6b4
Torrey <torreythms@gmail.com>
2022-01-18 15:05:41 -0500

    updated commit

---
+++ mindex-java-code-challenge/src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,45 @@
+package com.mindex.challenge.data;
+
+import java.util.Date;
+
+public class Compensation {
+	private Employee employee;
+	private Double salary;
+	private Date effectiveDate;
+	
+	public Compensation() {
+		
+	}
+	
+//	public Compensation(Employee employee, Double salary, Instant effectiveDate) {
+//		this.employee = employee;
+//		this.salary = salary;
+//		this.effectiveDate = effectiveDate;
+//	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+	
+	
+}
