57300d00c9435f22d10316b2bc35976cffcf245c
Torrey <torreythms@gmail.com>
2022-01-25 14:40:02 -0500

    clean up erranous files

--- src/main/java/com/mindex/challenge/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.mindex.challenge.service;
-
-import com.mindex.challenge.data.Employee;
-
-public interface EmployeeService {
-    Employee create(Employee employee);
-    Employee read(String id);
-    Employee update(Employee employee);
-}
